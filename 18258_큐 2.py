2ecd2b08c884376a87f3dc1ba653a56a14d6715d
mutjin08 <mutjin08@gmail.com>
2025-01-10 20:04:56 +0900

    25-01-09

---
+++ 18258_큐 2.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+n = int(input())
+q = deque([])
+for _ in range(n):
+    cmd = input().split()
+    if len(cmd)>1:
+        x = int(cmd[1])
+    cmd = cmd[0]
+
+    if cmd == "push":
+        q.append(x)
+    
+    elif cmd == "pop":
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+
+    elif cmd == "size":
+        print(len(q))
+
+    elif cmd == "empty":
+        if not q:
+            print(1)
+        else:
+            print(0)
+
+    elif cmd == "front":
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+
+    elif cmd == "back":
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
+
+    else:
+        continue
